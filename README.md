707f23e0cd59bdb2e4626723e3623f58ac1ec133
AhmadAlsous <ahmadalsous17@gmail.com>
2023-05-25 13:50:50 +0300

    First solution

---
+++ README.md
@@ -0,0 +1,2 @@
+# Karel Assignment
+This repository provides the solution for Atypon's Karel assignment.
---
+++ src/Homework.java
@@ -0,0 +1,86 @@
+import stanford.karel.SuperKarel;
+
+public class Homework extends SuperKarel {
+
+    public void run() {
+      int width = calcWidth();
+      moveToCenterCol(width);
+      int height = calcAndFillHeight();
+      int min=Math.min(width,height);
+      int squareLength=min-2;
+      moveToSquareStart(width,height);
+      fillInnerChamber(squareLength);
+      moveToCenterRow(squareLength);
+      fillWidth(width);
+    }
+    
+    private int calcWidth(){
+      int counter=1;
+      while(frontIsClear()){
+        move();
+        counter++;
+      }
+      return counter;
+    }
+    
+    private void moveToCenterCol(int width){
+      turnAround();
+      moveMultiple((width-1)/2);
+      turnRight();
+    }
+    
+    private int calcAndFillHeight(){
+      int counter=1;
+      putBeeper();
+      while(frontIsClear()){
+        move();
+        putBeeper();
+        counter++;
+      }
+      return counter;
+    }
+    
+    private void moveToSquareStart(int width,int height){
+      turnAround();
+      int steps=width>=height?1:(height-width)/2+1;
+      moveMultiple(steps);
+      turnLeft();
+    }
+    
+    private void fillInnerChamber(int length){
+      moveAndPutBeeper((length-1)/2);
+      for(int i=0;i<3;i++){
+        turnRight();
+        moveAndPutBeeper(length-1);
+      }
+      turnRight();
+      moveAndPutBeeper((length-1)/2);
+    }
+    
+    private void moveToCenterRow(int length){
+      turnRight();
+      moveMultiple((length-1)/2);
+    }
+    
+    private void fillWidth(int width){
+      turnRight();
+      moveAndPutBeeper((width-1)/2);
+      turnAround();
+      moveAndPutBeeper(width-1);
+    }
+    
+    private void moveMultiple(int n){
+      for(int i=0;i<n;i++)
+        move();
+    }
+    
+    private void moveAndPutBeeper(int n){
+      for(int i=0;i<n;i++){
+        move();
+        if(noBeepersPresent()){
+          putBeeper();
+        }
+      }
+    }
+
+}
