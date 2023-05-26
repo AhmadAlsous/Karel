f46d9c1dc7955c47e1272eec51812c5b2271c4da
AhmadAlsous <ahmadalsous17@gmail.com>
2023-05-26 18:59:42 +0300

    provided better code structure and comments

--- src/Homework.java
+++ src/Homework.java
@@ -1,107 +1,82 @@
 import stanford.karel.SuperKarel;
 
 public class Homework extends SuperKarel {
-  private int movesCounter;
-  private int height;
-  private int width;
-  private int squareLength;
+  private int movesCounter; // Keeps count of karel's total moves
+  private int height; // map height
+  private int width; // map width
+  private int squareLength; // length of the square that is formed by the 4 inner chambers
   
+  // the run method specifies what karel does after pressing start program
   public void run() {
+    initialize();
+    this.width = calcWidth();
+    this.height = calcAndFillHeight();
+    if(isSmallMap()){
+      return;
+    }
+    this.squareLength=Math.min(this.width,this.height)-2; // calculate square length
+    fillInnerChambers();
+    fillHorizontalOuterChambers();
+    System.out.println("Total moves required: "+ this.movesCounter); // print total moves
+  }
+  
+  // initialize beepers in bag and moves counter
+  private void initialize(){
     setBeepersInBag(1000);
     this.movesCounter=0;
-    this.width = calcWidth();
-    if(this.width<5){
-      System.out.println("The map is too small, width should be greater than 4.");
-      return;
-    }
-    moveToCenterCol();
-    this.height = calcAndFillHeight();
-    if(this.height<5){
-      System.out.println("The map is too small, height should be greater than 4.");
-      return;
-    }
-    boolean evenHeight = height%2==0;
-    this.squareLength=Math.min(this.width,this.height)-2;
-    moveToSquareStart();
-    fillInnerChamber();
-    moveToCenterRow();
-    fillWidth();
-    System.out.println("Total moves required: "+ movesCounter);
   }
   
   private int calcWidth(){
-    return calcAndPutBeeper(false);
+    return calcAndPutBeeper(false); // only calculates, no beepers
   }
   
-  private void moveToCenterCol(){
-    turnAround();
-    moveMultiple((this.width-1)/2);
-    turnRight();
-  }
-  
+  // calculate height and fill vertical outer chamber at once
   private int calcAndFillHeight(){
+    moveToCenterCol();
     putBeeper();
-    int height= calcAndPutBeeper(true);
-    if(isEven(this.width)){
+    int height = calcAndPutBeeper(true); // to fill while also calculating
+    if(isEven(this.width)){ // if width is even we need double vertical lines
       handleEven(height-1);
     }
     return height;
   }
   
-  private void moveToSquareStart(){
-    turnAround();
-    int steps=this.width>=this.height?1:(this.height-this.width)/2+1;
-    moveMultiple(steps);
-    turnLeft();
+  // handle special cases (small maps)
+  private boolean isSmallMap(){
+    // 5 is the smallest length allowed: 1 outer + 2 inner + 2 spaces away from walls
+    if(this.width<5 || this.height<5){
+      System.out.println("The map is too small, width and height should be greater than 4.");
+      return true;
+    }
+    return false;
   }
   
-  private void fillInnerChamber(){
-    moveAndPutBeeper((this.squareLength-1)/2);
-    for(int i=0;i<3;i++){
-      int steps=this.squareLength-1;
-      if(isEven(this.height) && !isEven(this.width) && i%2==0){
-        steps++;
-      }
-      if(isEven(this.width) && !isEven(this.height) && i%2==1){
-        steps++;
-      }
-      turnRight();
-      moveAndPutBeeper(steps);
+  private void fillInnerChambers(){
+    moveToSquareStart();
+    moveAndPutBeeper((this.squareLength-1)/2); // fill first half first side
+    for(int i=0;i<3;i++){ // fill second, third, and fourth sides
+      fillSquareSide(i);
     }
     turnRight();
-    moveAndPutBeeper((this.squareLength-1)/2);
+    moveAndPutBeeper((this.squareLength-1)/2); // fill second half first side
   }
   
-  private void moveToCenterRow(){
+  private void fillHorizontalOuterChambers(){
+    moveToCenterRow();
     turnRight();
-    moveMultiple((this.squareLength-1)/2);
-  }
-  
-  private void fillWidth(){
-    turnRight();
-    moveAndPutBeeper((this.width-1)/2);
+    moveAndPutBeeper((this.width-1)/2); // fill half the width
+    // if height is even we need double horizontal lines
     if (isEven(this.height)){
-      handleEven(this.width - 1);
-      handleEven((this.width-1)/2-1);
+      handleEven(this.width - 1); // fill second horizontal line
+      handleEven((this.width-1)/2-1); // fill second half of the width
     }
     else {
       turnAround();
-      moveAndPutBeeper(this.width - 1);
+      moveAndPutBeeper(this.width - 1); // fill second half of the width
     }
   }
   
-  private void moveMultiple(int n){
-    for(int i=0;i<n;i++)
-      move();
-  }
-  
-  private void moveAndPutBeeper(int n){
-    for(int i=0;i<n;i++){
-      move();
-      putBeeper();
-    }
-  }
-  
+  // calculates the length until it encounters a wall, can put beepers or not based on argument
   private int calcAndPutBeeper(boolean beeper){
     int counter=1;
     while(frontIsClear()){
@@ -114,28 +89,81 @@ public class Homework extends SuperKarel {
     return counter;
   }
   
-  private void putBeeperAndTurn(){
-    move();
-    putBeeper();
+  // moves karel to the center column (to start filling the vertical outer chambers)
+  private void moveToCenterCol(){
+    turnAround();
+    moveMultiple((this.width-1)/2); // move half the width
+    turnRight();
+  }
+  
+  // moves karel to the inner chambers start (to start filling the inner chambers)
+  private void moveToSquareStart(){
+    turnAround();
+    // since the inner chambers can't be adjacent to the walls, we move at least one from
+    // the wall, more if width is greater than height
+    int steps=this.width>=this.height?1:(this.height-this.width)/2+1;
+    moveMultiple(steps);
     turnLeft();
   }
   
-  private void handleEven(int length){
-    turnLeft();
-    putBeeperAndTurn();
-    moveAndPutBeeper(length);
+  private void fillSquareSide(int side){ // fills one side of the square
+    int steps=this.squareLength-1;
+    // cover the cases where only one of the width and height are even
+    if(isEven(this.height) && !isEven(this.width) && side%2==0){
+      steps++;
+    }
+    if(isEven(this.width) && !isEven(this.height) && side%2==1){
+      steps++;
+    }
+    turnRight();
+    moveAndPutBeeper(steps);
+  }
+  
+  private void moveAndPutBeeper(int n){
+    for(int i=0;i<n;i++){
+      move();
+      putBeeper();
+    }
   }
   
   private boolean isEven(int n){
     return n%2==0;
   }
   
+  // moves karel to the center row (to start filling the horizontal outer chambers)
+  private void moveToCenterRow(){
+    turnRight();
+    moveMultiple((this.squareLength-1)/2); // move half the square length
+  }
+  
+  // used when we need to turn and start second line of beepers when even
+  private void handleEven(int length){
+    turnLeft();
+    putBeeperAndTurn();
+    moveAndPutBeeper(length);
+  }
+
+  private void moveMultiple(int n){
+    for(int i=0;i<n;i++)
+      move();
+  }
+  
+  private void putBeeperAndTurn(){
+    move();
+    putBeeper();
+    turnLeft();
+  }
+  
+  // override the move method to print a counter each time karel moves
+  @Override
   public void move(){
     super.move();
     movesCounter++;
     System.out.println("Karel moves counter: "+ movesCounter);
   }
   
+  // override the putBeeper method to only put beepers if there are no beepers present
+  @Override
   public void putBeeper(){
     if(noBeepersPresent()){
       super.putBeeper();
