9a6e415b26d28c09db3eb37a000477a870acd4c6
AhmadAlsous <ahmadalsous17@gmail.com>
2023-05-26 17:47:46 +0300

    introduced some state variables to reduce method parameters

--- src/Homework.java
+++ src/Homework.java
@@ -1,87 +1,145 @@
 import stanford.karel.SuperKarel;
 
 public class Homework extends SuperKarel {
-
-    public void run() {
-      int width = calcWidth();
-      moveToCenterCol(width);
-      int height = calcAndFillHeight();
-      int min=Math.min(width,height);
-      int squareLength=min-2;
-      moveToSquareStart(width,height);
-      fillInnerChamber(squareLength);
-      moveToCenterRow(squareLength);
-      fillWidth(width);
+  private int movesCounter;
+  private int height;
+  private int width;
+  private int squareLength;
+  
+  public void run() {
+    setBeepersInBag(1000);
+    this.movesCounter=0;
+    this.width = calcWidth();
+    if(this.width<5){
+      System.out.println("The map is too small, width should be greater than 4.");
+      return;
     }
-    
-    private int calcWidth(){
-      return calcAndPutBeeper(false);
+    moveToCenterCol();
+    this.height = calcAndFillHeight();
+    if(this.height<5){
+      System.out.println("The map is too small, height should be greater than 4.");
+      return;
     }
-    
-    private void moveToCenterCol(int width){
-      turnAround();
-      moveMultiple((width-1)/2);
+    boolean evenHeight = height%2==0;
+    this.squareLength=Math.min(this.width,this.height)-2;
+    moveToSquareStart();
+    fillInnerChamber();
+    moveToCenterRow();
+    fillWidth();
+    System.out.println("Total moves required: "+ movesCounter);
+  }
+  
+  private int calcWidth(){
+    return calcAndPutBeeper(false);
+  }
+  
+  private void moveToCenterCol(){
+    turnAround();
+    moveMultiple((this.width-1)/2);
+    turnRight();
+  }
+  
+  private int calcAndFillHeight(){
+    putBeeper();
+    int height= calcAndPutBeeper(true);
+    if(isEven(this.width)){
+      handleEven(height-1);
+    }
+    return height;
+  }
+  
+  private void moveToSquareStart(){
+    turnAround();
+    int steps=this.width>=this.height?1:(this.height-this.width)/2+1;
+    moveMultiple(steps);
+    turnLeft();
+  }
+  
+  private void fillInnerChamber(){
+    moveAndPutBeeper((this.squareLength-1)/2);
+    for(int i=0;i<3;i++){
+      int steps=this.squareLength-1;
+      if(isEven(this.height) && !isEven(this.width) && i%2==0){
+        steps++;
+      }
+      if(isEven(this.width) && !isEven(this.height) && i%2==1){
+        steps++;
+      }
       turnRight();
+      moveAndPutBeeper(steps);
     }
-    
-    private int calcAndFillHeight(){
+    turnRight();
+    moveAndPutBeeper((this.squareLength-1)/2);
+  }
+  
+  private void moveToCenterRow(){
+    turnRight();
+    moveMultiple((this.squareLength-1)/2);
+  }
+  
+  private void fillWidth(){
+    turnRight();
+    moveAndPutBeeper((this.width-1)/2);
+    if (isEven(this.height)){
+      handleEven(this.width - 1);
+      handleEven((this.width-1)/2-1);
+    }
+    else {
+      turnAround();
+      moveAndPutBeeper(this.width - 1);
+    }
+  }
+  
+  private void moveMultiple(int n){
+    for(int i=0;i<n;i++)
+      move();
+  }
+  
+  private void moveAndPutBeeper(int n){
+    for(int i=0;i<n;i++){
+      move();
       putBeeper();
-      return calcAndPutBeeper(true);
     }
-    
-    private void moveToSquareStart(int width,int height){
-      turnAround();
-      int steps=width>=height?1:(height-width)/2+1;
-      moveMultiple(steps);
-      turnLeft();
-    }
-    
-    private void fillInnerChamber(int length){
-      moveAndPutBeeper((length-1)/2);
-      for(int i=0;i<3;i++){
-        turnRight();
-        moveAndPutBeeper(length-1);
+  }
+  
+  private int calcAndPutBeeper(boolean beeper){
+    int counter=1;
+    while(frontIsClear()){
+      move();
+      if(beeper){
+        putBeeper();
       }
-      turnRight();
-      moveAndPutBeeper((length-1)/2);
+      counter++;
     }
-    
-    private void moveToCenterRow(int length){
-      turnRight();
-      moveMultiple((length-1)/2);
+    return counter;
+  }
+  
+  private void putBeeperAndTurn(){
+    move();
+    putBeeper();
+    turnLeft();
+  }
+  
+  private void handleEven(int length){
+    turnLeft();
+    putBeeperAndTurn();
+    moveAndPutBeeper(length);
+  }
+  
+  private boolean isEven(int n){
+    return n%2==0;
+  }
+  
+  public void move(){
+    super.move();
+    movesCounter++;
+    System.out.println("Karel moves counter: "+ movesCounter);
+  }
+  
+  public void putBeeper(){
+    if(noBeepersPresent()){
+      super.putBeeper();
     }
-    
-    private void fillWidth(int width){
-      turnRight();
-      moveAndPutBeeper((width-1)/2);
-      turnAround();
-      moveAndPutBeeper(width-1);
-    }
-    
-    private void moveMultiple(int n){
-      for(int i=0;i<n;i++)
-        move();
-    }
-    
-    private void moveAndPutBeeper(int n){
-      for(int i=0;i<n;i++){
-        move();
-        if(noBeepersPresent()){
-          putBeeper();
-        }
-      }
-    }
-    
-    private int calcAndPutBeeper(boolean beeper){
-      int counter=1;
-      while(frontIsClear()){
-        move();
-        if(beeper){
-          putBeeper();
-        }
-        counter++;
-      }
-      return counter;
-    }
-
+  }
+  
 }
